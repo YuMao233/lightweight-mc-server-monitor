dbdcdcb14c7c8ba5616607af9b98f41ae4c6b447
unitwk <unitwk@outlook.com>
2023-06-12 21:14:03 +0800

    Feat: 同步修改项&构建

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     proxy: {
       // 接口地址代理
       '/api': {
-        target: 'http://localhost:3000/', // 接口的域名
+        target: 'http://localhost:80/', // 接口的域名
         secure: false, // 如果是https接口，需要配置这个参数
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
         rewrite: (path) => path.replace(/^\/api/, '/api')
